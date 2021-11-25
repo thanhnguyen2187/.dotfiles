e3b7b1a0b025152d1d033cbc05e02add5e206af5
Thanh Nguyen <thanhnguyen2187@gmail.com>
2021-11-25 20:09:37 +0700

    Add coq_nvim, nvim-lspconfig, conjure, vim-racket, fzf, fzf.vim, vim-obsession, and vim-better-whitespace

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,7 +23,10 @@ vim.api.nvim_exec(
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim'
-  use 'ms-jpq/coq_nvim'
+  use {
+    'ms-jpq/coq_nvim',
+    branch = 'coq',
+  }
   vim.g.coq_settings = {
     auto_start = true,
   }
@@ -54,9 +57,68 @@ require('packer').startup(function()
     }
   })
 
+  use 'neovim/nvim-lspconfig'
+  local nvim_lsp = require('lspconfig')
+  local coq = require('coq')
+
+  -- use 'ray-x/lsp_signature.nvim'
+  -- require('lsp_signature').setup()
+
+  local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  end
+
+  use 'olical/conjure'
+
+  use 'wlangstroth/vim-racket'
+  nvim_lsp.racket_langserver.setup(
+    coq.lsp_ensure_capabilities({
+      on_attach = on_attach,
+      flags = {
+        debounce_text_changes = 150,
+      }
+    })
+  )
+
+  use {
+    'junegunn/fzf',
+    run = './install --bin',
+  }
+  use 'junegunn/fzf.vim'
   use 'tpope/vim-commentary'
   use 'tpope/vim-repeat'
   use 'tpope/vim-surround'
+  use 'tpope/vim-obsession'
   use 'tommcdo/vim-exchange'
   use 'inkarkat/vim-ReplaceWithRegister'
+  use 'ntpeters/vim-better-whitespace'
+  vim.g.better_whitespace_enabled = 1
+  vim.g.strip_whitespace_on_save = 1
+  vim.g.strip_whitespace_confirm = 0
 end)
